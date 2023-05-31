9077d56d2e19d9cd4469610d0881d29e15f7d036
Andrii Bodnar <andrii.bodnar@crowdin.com>
2023-05-31 12:22:29 +0300

    chore: version 1.10.1

--- crowdin_api/__init__.py
+++ crowdin_api/__init__.py
@@ -2,7 +2,7 @@ from crowdin_api.client import CrowdinClient
 
 __all__ = ["CrowdinClient"]
 __author__ = "Crowdin"
-__version__ = "1.10.0"
+__version__ = "1.10.1"
 
 __pdoc__ = {
     "tests": False,
