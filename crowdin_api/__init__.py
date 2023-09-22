fba08fcf7e94754d00620b52db5f744c4302d2a8
andrii-bodnar <andrii-bodnar@users.noreply.github.com>
2023-09-22 14:04:21 +0000

    chore: version 1.12.1 [skip ci]

--- crowdin_api/__init__.py
+++ crowdin_api/__init__.py
@@ -2,7 +2,7 @@ from crowdin_api.client import CrowdinClient
 
 __all__ = ["CrowdinClient"]
 __author__ = "Crowdin"
-__version__ = "1.12.0"
+__version__ = "1.12.1"
 
 __pdoc__ = {
     "tests": False,
