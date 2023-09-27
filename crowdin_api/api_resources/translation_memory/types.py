38342e7f697b233b25b7d291d23b5a8661c3894f
OmAxiani0 <75031769+OmAximani0@users.noreply.github.com>
2023-09-27 21:57:20 +0530

    feat: Translation Memory Segments API Suppport
    Signed-off-by: OmAxiani0 <75031769+OmAximani0@users.noreply.github.com>

--- crowdin_api/api_resources/translation_memory/types.py
+++ crowdin_api/api_resources/translation_memory/types.py
@@ -1,7 +1,11 @@
 from typing import Any
 
 from crowdin_api.api_resources.enums import PatchOperation
-from crowdin_api.api_resources.translation_memory.enums import TranslationMemoryPatchPath
+from crowdin_api.api_resources.translation_memory.enums import (
+    TranslationMemoryPatchPath,
+    TranslationMemorySegmentRecordOperation,
+    TranslationMemorySegmentRecordOperationPath,
+)
 from crowdin_api.typing import TypedDict
 
 
@@ -9,3 +13,25 @@ class TranslationMemoryPatchRequest(TypedDict):
     value: Any
     op: PatchOperation
     path: TranslationMemoryPatchPath
+
+
+class TranslationMemorySegmentRecord(TypedDict):
+    languageId: str
+    text: str
+
+
+class TranslationMemorySegmentRecordOperationAdd(TypedDict):
+    op: TranslationMemorySegmentRecordOperation
+    path: TranslationMemorySegmentRecordOperationPath
+    value: TranslationMemorySegmentRecord
+
+
+class TranslationMemorySegmentRecordOperationReplace(TypedDict):
+    op: TranslationMemorySegmentRecordOperation
+    path: TranslationMemorySegmentRecordOperationPath
+    value: str
+
+
+class TranslationMemorySegmentRecordOperationRemove(TypedDict):
+    op: TranslationMemorySegmentRecordOperation
+    path: TranslationMemorySegmentRecordOperationPath
