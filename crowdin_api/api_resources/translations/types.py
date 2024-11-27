1fa4b5ba6d7edefdc551e2be576fca28ed0076a3
Iván Luna <133716241+imprvhub@users.noreply.github.com>
2024-11-27 16:26:00 -0300

    feat: add addToTm parameter support for Upload Translation API (#185)

--- crowdin_api/api_resources/translations/types.py
+++ crowdin_api/api_resources/translations/types.py
@@ -1,7 +1,5 @@
-from typing import Iterable
-
+from typing import Iterable, Optional
 from crowdin_api.typing import TypedDict
-
 from crowdin_api.api_resources.translations.enums import PreTranslationEditOperation
 
 
@@ -13,3 +11,12 @@ class EditPreTranslationScheme(TypedDict):
     op: PreTranslationEditOperation
     path: str
     value: str
+
+
+class UploadTranslationRequest(TypedDict):
+    storageId: int
+    fileId: int
+    importEqSuggestions: Optional[bool]
+    autoApproveImported: Optional[bool]
+    translateHidden: Optional[bool]
+    addToTm: Optional[bool]
