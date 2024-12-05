24bc403be39117933a61d90b6717488e2c5f75f4
Martin Chrástek <chrastek12@gmail.com>
2024-12-05 14:15:47 +0100

    Update test_parser

--- crowdin_api/tests/test_parser.py
+++ crowdin_api/tests/test_parser.py
@@ -6,27 +6,9 @@ from crowdin_api.parser import dumps, loads
 from crowdin_api.sorting import Sorting, SortingOrder, SortingRule
 
 
-class TestEnum(Enum):
-    ONE = "one"
-    TWO = "two"
-
-
 @pytest.mark.parametrize(
     "in_value, out_value",
     (
-        (
-            '{"orderBy": "one desc,two"}',
-            {
-                "orderBy": str(
-                    Sorting(
-                        [
-                            SortingRule(TestEnum.ONE, SortingOrder.DESC),
-                            SortingRule(TestEnum.TWO),
-                        ]
-                    )
-                )
-            },
-        ),
         ('{"int": 1}', {"int": 1}),
         ('{"float": 3.14}', {"float": 3.14}),
         ('{"string": "some string"}', {"string": "some string"}),
@@ -94,3 +76,22 @@ def test_parser(in_value, out_value):
 
     dumps_result = dumps(load_result)
     assert dumps_result == in_value
+
+
+class TestEnum(Enum):
+    ID = "id"
+    CREATED_AT = "createdAt"
+
+
+def test_sorting_serialization():
+    sorting = Sorting(
+        [
+            SortingRule(TestEnum.ID, SortingOrder.DESC),
+            SortingRule(TestEnum.CREATED_AT, SortingOrder.ASC),
+        ]
+    )
+
+    result = dumps({"orderBy": sorting})
+    expected = '{"orderBy": "id desc,createdAt asc"}'
+
+    assert result == expected
