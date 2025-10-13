22a943e83ca2b4a608d2e40f1009175bbd58b490
Faiza Jibril <73698470+FaizaJibril@users.noreply.github.com>
2025-10-13 15:48:05 +0100

    feat(tasks): add task comments support (#216)

--- crowdin_api/api_resources/tasks/resource.py
+++ crowdin_api/api_resources/tasks/resource.py
@@ -27,6 +27,7 @@ from crowdin_api.api_resources.tasks.types import (
     TaskSettingsTemplateLanguages,
 )
 from crowdin_api.sorting import Sorting
+from crowdin_api.api_resources.tasks.types import TaskCommentPatchRequest
 
 
 class TasksResource(BaseResource):
@@ -933,6 +934,133 @@ class TasksResource(BaseResource):
         params.update(self.get_page_params(page=page, offset=offset, limit=limit))
 
         return self._get_entire_data(method="get", path="user/tasks", params=params)
+
+    # Task Comments
+    def get_task_comments_path(
+        self,
+        projectId: int,
+        taskId: int,
+        taskCommentId: Optional[int] = None,
+    ):
+        if taskCommentId is not None:
+            return f"projects/{projectId}/tasks/{taskId}/comments/{taskCommentId}"
+
+        return f"projects/{projectId}/tasks/{taskId}/comments"
+
+    def list_task_comments(
+        self,
+        taskId: int,
+        projectId: Optional[int] = None,
+        orderBy: Optional[Sorting] = None,
+        page: Optional[int] = None,
+        offset: Optional[int] = None,
+        limit: Optional[int] = None,
+    ):
+        """
+        List Task Comments.
+
+        Link to documentation:
+        https://developer.crowdin.com/api/v2/#operation/api.projects.tasks.comments.getMany
+        """
+
+        projectId = projectId or self.get_project_id()
+        params = {"orderBy": orderBy}
+        params.update(self.get_page_params(page=page, offset=offset, limit=limit))
+
+        return self._get_entire_data(
+            method="get",
+            path=self.get_task_comments_path(projectId=projectId, taskId=taskId),
+            params=params,
+        )
+
+    def add_task_comment(
+        self,
+        text: str,
+        taskId: int,
+        projectId: Optional[int] = None,
+    ):
+        """
+        Add Task Comment.
+
+        Link to documentation:
+        https://developer.crowdin.com/api/v2/#operation/api.projects.tasks.comments.post
+        """
+
+        projectId = projectId or self.get_project_id()
+
+        return self.requester.request(
+            method="post",
+            path=self.get_task_comments_path(projectId=projectId, taskId=taskId),
+            request_data={"text": text},
+        )
+
+    def get_task_comment(
+        self,
+        taskCommentId: int,
+        taskId: int,
+        projectId: Optional[int] = None,
+    ):
+        """
+        Get Task Comment.
+
+        Link to documentation:
+        https://developer.crowdin.com/api/v2/#operation/api.projects.tasks.comments.get
+        """
+
+        projectId = projectId or self.get_project_id()
+
+        return self.requester.request(
+            method="get",
+            path=self.get_task_comments_path(
+                projectId=projectId, taskId=taskId, taskCommentId=taskCommentId
+            ),
+        )
+
+    def delete_task_comment(
+        self,
+        taskCommentId: int,
+        taskId: int,
+        projectId: Optional[int] = None,
+    ):
+        """
+        Delete Task Comment.
+
+        Link to documentation:
+        https://developer.crowdin.com/api/v2/#operation/api.projects.tasks.comments.delete
+        """
+
+        projectId = projectId or self.get_project_id()
+
+        return self.requester.request(
+            method="delete",
+            path=self.get_task_comments_path(
+                projectId=projectId, taskId=taskId, taskCommentId=taskCommentId
+            ),
+        )
+
+    def edit_task_comment(
+        self,
+        taskCommentId: int,
+        data: Iterable[TaskCommentPatchRequest],
+        taskId: int,
+        projectId: Optional[int] = None,
+    ):
+        """
+        Edit Task Comment.
+
+        Link to documentation:
+        https://developer.crowdin.com/api/v2/#operation/api.projects.tasks.comments.patch
+        """
+
+        projectId = projectId or self.get_project_id()
+
+        return self.requester.request(
+            method="patch",
+            path=self.get_task_comments_path(
+                projectId=projectId, taskId=taskId, taskCommentId=taskCommentId
+            ),
+            request_data=data,
+        )
 
     def edit_task_archived_status(
         self, taskId: int, isArchived: bool = True, projectId: Optional[int] = None
